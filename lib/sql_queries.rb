f8868d151fc384f714d8cdf8328ba233586fcd53
derrick b <dbozich1@gmail.com>
2018-06-16 20:40:53 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ end
 
 def counts_number_of_bears_with_goofy_temperaments
   # 'SELECT temperament FROM bears WHERE temperament = goofy ;'
-  '  SELECT COUNT(temperament) FROM bears;'
+  '  SELECT COUNT(temperament = goofy) FROM bears;'
 
 end
 
